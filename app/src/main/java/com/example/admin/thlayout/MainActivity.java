d914174463d47fe0f732b46cad695766eb87b0b7
Lê Cẩm <lecam83it@gmail.com>
2017-08-02 21:21:21 +0700

    commit changed

--- app/src/main/java/com/example/admin/thlayout/MainActivity.java
+++ app/src/main/java/com/example/admin/thlayout/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.admin.thlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listview_practice);
+        lv = (ListView) findViewById(R.id.listView);
+
+        //Tạo mảng gán giá trị các thông số listView
+        String [] name = {"A","B","C","D","E"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
+        lv.setAdapter(arrayAdapter);
     }
 }
